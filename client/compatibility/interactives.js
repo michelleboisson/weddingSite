6db527b7de6c4512aae69c070a5ff0a9039e5170
Michelle Boisson <michelle.boisson@gmail.com>
2014-02-23 15:14:05 -0500

    fixed nav on pages

--- client/compatibility/interactives.js
+++ client/compatibility/interactives.js
@@ -121,6 +121,21 @@ letsGetRolling = function(){
 	//$("#RSVPbtn").on('click', RSVPanimation.toggleSlide);
 	//$("#closebtn").on('click', RSVPanimation.slideup);
 
+$('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top - 50
+          }, 1000);
+          return false;
+        }
+      }
+    });
+
+
 }
 
 
